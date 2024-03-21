e8575a73c5b55e90e3a4b69b1f307f1aa1195f7a
aman singh <amansingh21510@gmail.com>
2024-03-22 02:41:23 +0530

    google and meta

--- v1.1/Google/data_collector.py
+++ v1.1/Google/data_collector.py
@@ -1,11 +1,9 @@
 from alpha_vantage.timeseries import TimeSeries
 
-ticker='AAPL'
+ticker='GOOG'
 API_key = 'WSKF50ODKWY4WP1O'
 ts = TimeSeries(key= API_key, output_format='pandas')
 res = ts.get_daily(ticker, outputsize='full')
 
 df=res[0]
 df.to_csv(f'./data/{ticker}.csv')
-
-
